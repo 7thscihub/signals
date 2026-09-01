ac6ad4fc477e3f9f26ed4ad6ba097620e8066bb0
7thscihub <7thscihub@gmail.com>
2026-09-01 20:18:04 +0300

    used traceback format

--- src/main.py
+++ src/main.py
@@ -6,24 +6,27 @@ from mooneazy.scripts.scalper import get_scalping_signals
 def main(context):
     errors = {}
     latest_signals = []
-    signals, scalper_errors = get_scalping_signals()
-    errors['scalper_errors'] = scalper_errors or None
-    if signals:
+    try:
+        signals, scalper_errors = get_scalping_signals()
+        errors['scalper_errors'] = scalper_errors or None
+    except Exception as e:
+        context.log(traceback.format_exc())
+        if signals:
         try:
             send_push_notifications(signals)
         except Exception as e:
             errors['push_notification_errors'] = e
-            context.log(e)
+            context.log(traceback.format_exc())
     try:
         # update signals database and return latest_signals
         db_signals, db_errors = update_signals(signals)
         if db_signals:
             latest_signals = db_signals
-        errors['updata_signals_errors'] = db_errors or None
+        errors['updated_signals_errors'] = db_errors or None
 
     except Exception as e:
-        errors['db_errors'] = f"ERROR ADDING SIGNALS TO DB \n{e}"
-    
+        context.log(traceback.format_exc())
+
     return context.res.json({
         "signals": latest_signals or signals,
         "errors": errors if errors else None 
