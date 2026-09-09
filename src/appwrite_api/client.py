2381fd8fb9d72f449efdc3fdd651cd80764348a4
7thscihub <7thscihub@gmail.com>
2026-09-09 20:21:57 +0300

    fixed endpoint

--- src/appwrite_api/client.py
+++ src/appwrite_api/client.py
@@ -7,14 +7,14 @@ def get_client():
     ENDPOINT = os.environ.get("APPWRITE_FUNCTION_API_ENDPOINT", None)
     PROJECT_ID = os.environ.get("APPWRITE_FUNCTION_PROJECT_ID", None)
     API_KEY = os.environ.get("APPWRITE_FUNCTION_API_KEY", None)
-    if not ENPOINT:
+    if not ENDPOINT:
         raise ValueError('Missing ENPOINT')
     if not PROJECT_ID:
         raise ValueError("Missing PROJECT_ID")
     if not API_KEY:
         raise ValueError("Missing API_KEY")
 
-    client.set_endpoint(ENPOINT)
+    client.set_endpoint(ENDPOINT)
     client.set_project(PROJECT_ID)
     client.set_key(API_KEY)
     return client
