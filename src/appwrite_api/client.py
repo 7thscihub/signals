d1d2803c6aed910b0116023c7e20ef66ff0f8dca
7thscihub <7thscihub@gmail.com>
2026-09-09 20:07:28 +0300

    checking for envs

--- src/appwrite_api/client.py
+++ src/appwrite_api/client.py
@@ -4,8 +4,18 @@ from appwrite.client import Client
 
 def get_client():
     client = Client()
-    client.set_endpoint(os.environ.get("APPWRITE_FUNCTION_API_ENDPOINT"))
-    client.set_project(os.environ.get("APPWRITE_FUNCTION_PROJECT_ID"))
-    client.set_key(os.environ.get("APPWRITE_FUNCTION_API_KEY"))
+    ENDPOINT = os.environ.get("APPWRITE_FUNCTION_API_ENDPOINT", None)
+    PROJECT_ID = os.environ.get("APPWRITE_FUNCTION_PROJECT_ID", None)
+    API_KEY = os.environ.get("APPWRITE_FUNCTION_API_KEY", None)
+    if not ENPOINT:
+        raise ValueError('Missing ENPOINT')
+    if not PROJECT_ID:
+        raise ValueError("Missing PROJECT_ID")
+    if not API_KEY:
+        raise ValueError("Missing API_KEY")
+
+    client.set_endpoint(ENPOINT)
+    client.set_project(PROJECT_ID)
+    client.set_key(API_KEY(API_KEY)
     return client
 
