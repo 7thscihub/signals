d10ed9c552fec82b0c93c0a04253a1a33093fe46
7thscihub <7thscihub@gmail.com>
2026-09-18 16:52:11 +0300

    uncommented main function

--- src/results.py
+++ src/results.py
@@ -3,19 +3,19 @@ from mooneazy.trading.results import get_results
 from .appwrite_api.db import update_results, get_pending_signals
 from .appwrite_api.messages import send_push_notifications
 
-#
-# def main(context):
-#     new_results = []
-#     errors = None
-#     try:
-#         pending_signals = get_pending_signals()
-#         new_results = get_results(pending_signals)
-#         send_push_notifications(results)
-#         upddate_results(results)
-#     except Exception as e:
-#         errors = traceback.format_exc()
-#         context.log(errors)
-#     if erros:
-#         return context.res(errors)
-#     return context.res(new_results)
+
+ def main(context):
+     new_results = []
+     errors = None
+     try:
+         pending_signals = get_pending_signals()
+         new_results = get_results(pending_signals)
+         send_push_notifications(results)
+         upddate_results(results)
+     except Exception as e:
+         errors = traceback.format_exc()
+         context.log(errors)
+     if erros:
+         return context.res(errors)
+     return context.res(new_results)
 
