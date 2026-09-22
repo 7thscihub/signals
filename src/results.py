a90bef7b196ca27d7364bc0e782a245203cfc1bf
7thscihub <7thscihub@gmail.com>
2026-09-22 15:57:26 +0300

    returning results

--- src/results.py
+++ src/results.py
@@ -13,11 +13,9 @@ def main(context):
     }
     try:
         pending_signals = get_pending_signals()
-        return context.res.json(pending_signals)
-
         results['pending_signals'] = pending_signals
         updated_signals = get_updated_signals(pending_signals)
-        
+        return context.res.json(results)
         if updated_signals:
             results['updated_signals'] = updated_signals
             send_push_notifications(updated_signals)
